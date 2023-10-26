a2b60ad125142bf32b31a54cbbec66bc7195747f
mukeshr-29 <mukeshr2911@gmail.com>
2023-10-26 22:37:40 +0530

    vars

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call() {
     withSonarQubeEnv('sonarqube') {
-        sh ''' $SCANNER_HOME/bin/sonarqube -Dsonar.projectName=youtube-project -Dsonar.projectKey=youtube-project '''
+        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube-project -Dsonar.projectKey=youtube-project '''
     }
 }
